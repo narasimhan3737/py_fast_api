f20a48a9a5dc7cd61f18d7b205e570d9185c563f
Hari Narasimhan <narasimhan3737@gmail.com>
2025-05-11 15:04:16 +0100

    Added pytest

--- src/main.py
+++ src/main.py
@@ -21,3 +21,7 @@ async def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+@app.post("/items")
+async def create_item(item: Item):
+    return {"item_name": item.name, "item_price": item.price, "item_is_offer": item.is_offer}
